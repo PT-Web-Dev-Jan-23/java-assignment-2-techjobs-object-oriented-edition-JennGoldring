8f68ce2fa69a605776eb6faaff9b28bc4b517a60
JennGoldring <jenngoldring@yahoo.com>
2023-05-30 23:01:28 -0500

    Test Commit

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -9,17 +9,17 @@ import static org.junit.Assert.assertNotEquals;
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
-public class JobTest {
-    @Test
-    public void testSettingJobId() {
-        Job job1 = new Job();
-        Job job2 = new Job();
-
-        assertNotEquals(job1.getId(), job2.getId());
-    }
-
-
+//public class JobTest {
+//    @Test
+////    public void testSettingJobId() {
+////        Job job1 = new Job();
+////        Job job2 = new Job();
+////
+////        assertNotEquals(job1.getId(), job2.getId());
+////    }
 
 
 
-}
+
+
+
