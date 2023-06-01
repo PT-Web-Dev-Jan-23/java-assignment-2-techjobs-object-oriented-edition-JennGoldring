8421b9deada37aeca8579034da1b206c8d99716b
JennGoldring <jenngoldring@yahoo.com>
2023-05-31 22:37:43 -0500

    First JobTest

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,25 +1,27 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.Test;
-import static org.junit.Assert.assertNotEquals;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
-//public class JobTest {
-//    @Test
-////    public void testSettingJobId() {
-////        Job job1 = new Job();
-////        Job job2 = new Job();
-////
-////        assertNotEquals(job1.getId(), job2.getId());
-////    }
+public class JobTest {
 
+    @Test
+    public void testSettingJobID() {
+        Job job1 = new Job();
+        Job job2 = new Job();
 
+        assertNotEquals(job1.getId(), job2.getId());
+    }
 
+    @Test
 
-
-
+}
