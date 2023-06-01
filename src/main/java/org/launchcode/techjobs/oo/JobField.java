1ca4c8962278c07f15902e5c837fe1b81f5feedf
JennGoldring <jenngoldring@yahoo.com>
2023-06-01 09:15:26 -0500

    added JobField class

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,26 @@
+package org.launchcode.techjobs.oo;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
